fa982ca0e12feb03eb469d8ed379b0f0e4102f58
sfrerix <stefan.frerix@gmail.com>
2024-02-29 11:56:07 +0100

    Bugfix: Umsatzexport hat nicht mehr funktioniert.

--- src/projector_backend/app.py
+++ src/projector_backend/app.py
@@ -433,8 +433,9 @@ def create_buchungen_export():
 @app.route('/exportUmsaetze', methods=["GET"])
 def create_umsaetze_export():
     psp = request.args.get('psp')
+    booking_dtos: [BookingDTO] = pservice.get_bookings_for_psp(psp, False)
     filename_umsaetze = eh.export_umsaetze(psp, pservice.get_ma_bookings_summary_for_psp(psp, False),
-                                           pservice.get_bookings_summary_for_psp_by_month(psp, False),
+                                           pservice.get_bookings_summary_for_psp_by_month(booking_dtos, False),
                                            pservice.get_project_by_psp(psp, False).volumen)
     file_path_umsaetze = os.path.join(os.getcwd(), 'exports', filename_umsaetze)
     return send_file(file_path_umsaetze, as_attachment=True)
